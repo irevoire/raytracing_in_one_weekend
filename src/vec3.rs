dbc9e58d77fc774f6c4d0bc7983e48ac46b24dff
Thomas Campistron <irevoire@hotmail.fr>
2020-08-25 22:06:29 +0200

    8.3 Using gamma correction for accurate color intensity

--- src/vec3.rs
+++ src/vec3.rs
@@ -74,9 +74,12 @@ impl Vec3 {
     pub fn color(self, samples_per_pixel: usize) -> String {
         let scale = 1. / samples_per_pixel as f64;
 
-        let r = self.x * scale;
-        let g = self.y * scale;
-        let b = self.z * scale;
+        // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+        let r = (self.x * scale).sqrt();
+        let g = (self.y * scale).sqrt();
+        let b = (self.z * scale).sqrt();
+
+        // Write the translated [0,255] value of each color component.
         format!(
             "{} {} {}",
             (256. * r.clamp(0., 0.999)) as u64,
